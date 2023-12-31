6b692430c169571861c282be747ee22463125062
Kobi Hari <kobi2294@yahoo.com>
2023-12-31 11:28:05 +0200

    we're done with selectors

--- project/ngrx-new/src/app/app.component.ts
+++ project/ngrx-new/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SharedModule } from './shared.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { QuestionPresenterComponent } from './components/question-presenter/question-presenter.component';
@@ -6,6 +6,8 @@ import { Question } from './models/question.model';
 import { ProgressComponent } from './components/progress/progress.component';
 import { DoneComponent } from './components/done/done.component';
 import { randomColorQuestion } from './services/helpers';
+import { Store } from '@ngrx/store';
+import { quizFeature } from './redux/quiz.feature';
 
 @Component({
   selector: 'app-root',
@@ -15,8 +17,8 @@ import { randomColorQuestion } from './services/helpers';
   imports: [SharedModule, ToolbarComponent, QuestionPresenterComponent, ProgressComponent, DoneComponent],
 })
 export class AppComponent {
-  question: Question = randomColorQuestion();
-
+  store = inject(Store);
+  question$ = this.store.select(quizFeature.selectCurrentQuestion);
   constructor() {
   }
 
