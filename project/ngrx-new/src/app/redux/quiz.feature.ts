4a57febfdb9bda4d5be20e405fc5ee4a3a224fec
Kobi Hari <kobi2294@yahoo.com>
2023-12-31 11:06:45 +0200

    created initial selectors

--- project/ngrx-new/src/app/redux/quiz.feature.ts
+++ project/ngrx-new/src/app/redux/quiz.feature.ts
@@ -1,7 +1,27 @@
-import { createFeature } from "@ngrx/store";
-import { quizReducer } from "./quiz.reducer";
+import {
+  createFeature,
+  createFeatureSelector,
+  createSelector,
+} from '@ngrx/store';
+import { quizReducer } from './quiz.reducer';
+import { QuizState } from '../models/quiz-state.model';
 
 export const quizFeature = createFeature({
-    name: 'quiz', 
-    reducer: quizReducer
+  name: 'quiz',
+  reducer: quizReducer,
+  extraSelectors: ({ selectAnswers, selectQuestions }) => {
+    const selectCurrentQuestionIndex = createSelector(
+      selectAnswers,
+      (all) => all.length
+    );
+    const selectCurrentQuestion = createSelector(
+      selectQuestions,
+      selectCurrentQuestionIndex,
+      (all, index) => all[index]
+    );
+    return {
+        selectCurrentQuestionIndex, 
+        selectCurrentQuestion
+    };
+  },
 });
